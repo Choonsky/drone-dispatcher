686f9dc20585836a3d3a00633185f8ea9323bf25
Stanislav Nemirovsky <choonsky@gmail.com>
2025-02-27 20:35:22 +0600

    Some JUnit testing 2

--- src/test/java/com/nemirovsky/dronedispatcher/IntegrationTest.java
+++ src/test/java/com/nemirovsky/dronedispatcher/IntegrationTest.java
@@ -4,13 +4,20 @@ import com.nemirovsky.dronedispatcher.model.Drone;
 import com.nemirovsky.dronedispatcher.repository.DroneJpaRepository;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class IntegrationTest {
+
+    @Mock
+    DroneJpaRepository repo2;
 
     @MockBean
     private DroneJpaRepository droneRepository;
@@ -18,6 +25,17 @@ public class IntegrationTest {
     @Test
     @Disabled
     public void loadServiceTest() {
+
+
+        DroneJpaRepository repo1 = Mockito.mock(DroneJpaRepository.class);
+        Mockito.when(repo1.count()).thenReturn(5L);
+        Mockito.when(repo2.findById("1")).thenReturn(Optional.of(new Drone()));
+
+        long userCount = repo1.count();
+
+        assertEquals(5L, userCount);
+        // Mockito.verify(repo1).count();
+
         Drone d = droneRepository.findMaxIdDrone();
         assert d != null;
         assert d.getId() != null;
